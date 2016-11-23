67b419638b16163bdf6e42372150657b4906b4ae
jah4ck <johann.simon.web@gmail.com>
2016-11-23 06:57:24 +0100

    Outils d'appel de la dll pour génération de log

---
+++ TraceLog/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraceLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                GeneLog.GeneLog.Log(args[0], Convert.ToInt32(args[1]), args[2]);
+            }
+            catch (Exception err)
+            {
+
+                
+            }
+            
+        }
+    }
+}
