0eaca18d74baad5cfff95f8226d0c44b31c26ee8
jah4ck <johann.simon.web@gmail.com>
2016-12-13 11:34:02 +0100

    utilisation d'une nouvelle dll TraceLogAsync.dll
    l'utilisation de cette appli permet au service de passer par le dll

--- TraceLog/Program.cs
+++ TraceLog/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TraceLogAsync;
 
 namespace TraceLog
 {
@@ -12,12 +14,20 @@ namespace TraceLog
         {
             try
             {
-                GeneLog.GeneLog.Log(args[0], Convert.ToInt32(args[1]), args[2]);
+                //GeneLog.GeneLog.Log(args[0], Convert.ToInt32(args[1]), args[2]);
+                LogWriter write = LogWriter.Instance;
+                write.WriteToLog(args[0], Convert.ToInt32(args[1]), args[2]);
             }
             catch (Exception err)
             {
+                string NameDate = DateTime.Now.ToString("yyyyMMdd");
+                string Date = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
+                using (StreamWriter writer = File.AppendText(@"C:\ProgramData\CtrlPc\LOG\TraceLog_ERREUR_" + NameDate + ".log"))
+                {
+                    writer.WriteLine(Date + "     " + "TraceLog.exe" + "     " + "ERREUR : " + args[0]);
+                    writer.WriteLine(Date + "     " + "TraceLog.exe" + "     " + "ERREUR : " + err.Message);
 
-                
+                }
             }
             
         }
